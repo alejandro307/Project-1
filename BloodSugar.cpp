ee1190512753391affd0d7bf98356346264161d8
alejandro307 <37565081+alejandro307@users.noreply.github.com>
2018-10-28 19:22:01 -0600

    Add files via upload

---
+++ BloodSugar.cpp
@@ -0,0 +1,115 @@
+//Alejandro Bermudez
+
+#include "pch.h"
+#include "bloodSugarDay.h"
+#include <iostream>
+using std::cout;
+using std::endl;
+
+
+BloodSugarDay::BloodSugarDay(int weekday)
+{
+	dayOfWeek = weekday;
+	count = 0;
+	fence = first = last = new Entry;
+}
+
+BloodSugarDay::BloodSugarDay()
+{
+	dayOfWeek = 1;
+	count = 0;
+	fence = first = last = new Entry;
+}
+
+
+void BloodSugarDay::addEntry(int entry)
+{
+	fence = last;
+	fence->data = entry;
+	fence->next = new Entry;
+	last = fence = fence->next;
+	count++;
+}
+
+
+int BloodSugarDay::sumEntries()
+{
+	fence = first;
+	int sum(0);
+	if (count > 0)
+	{
+		while (fence != last)
+		{
+			sum += fence->data;
+			fence = fence->next;
+		}
+	}
+	return sum;
+}
+
+int BloodSugarDay::computeMax()
+{
+	fence = first;
+	int max(0);
+	if (count > 0)
+	{
+		while (fence != last)
+		{
+			if (fence->data > max)
+			{
+				max = fence->data;
+				fence = fence->next;
+			}
+			else
+			{
+				fence = fence->next;
+			}
+		}
+	}
+	return max;
+}
+
+int BloodSugarDay::computeMin()
+{
+	fence = first;
+	int min(fence->data);
+	fence = first;
+	while (fence != last)
+	{
+		if (fence->data < min)
+		{
+			min = fence->data;
+			fence = fence->next;
+		}
+		else
+		{
+			fence = fence->next;
+		}
+	}
+	return min;
+}
+
+int BloodSugarDay::getCount()
+{
+	return count;
+}
+
+void BloodSugarDay::printDay()
+{
+	cout << "\n\n\tEntries for day " << (dayOfWeek + 1) << ":" << endl << "++++++++++++++++++++++++++++++++++\n";
+	if (count == 0)
+	{
+		cout << "No data found for day.";
+	}
+	else
+	{
+		fence = first;
+		for (int i = 0; i < count; i++)
+		{
+			cout << "Entry " << i + 1 << ": \t" << fence->data << endl;
+			fence = fence->next;
+		}
+		cout << endl << "Daily Maximum: " << computeMax() << endl << "Daily Minimum: " << computeMin()
+			<< endl << "Daily Sum: " << sumEntries() << endl;
+	}
+}
