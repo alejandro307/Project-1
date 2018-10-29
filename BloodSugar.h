9884c9cb44da79634eb909c553f8ca49b569ef84
alejandro307 <37565081+alejandro307@users.noreply.github.com>
2018-10-28 19:25:01 -0600

    Add files via upload

---
+++ BloodSugar.h
@@ -0,0 +1,33 @@
+//Alejandro Bermudez
+
+#pragma once
+#include <string>
+using std::string;
+
+class Entry {
+public:
+	int data;
+	Entry * next;
+};
+
+class BloodSugarDay {
+private:
+	int dayOfWeek;
+	int count;
+	Entry * first;
+	Entry * fence;
+	Entry * last;
+
+
+public:
+	BloodSugarDay(int weekday);
+	BloodSugarDay();
+
+	void addEntry(int entry);
+
+	int sumEntries();
+	int computeMax();
+	int computeMin();
+	int getCount();
+	void printDay();
+}; 
